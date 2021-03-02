e8dc0488e804921c2ac2e068fd56a07edb04cd60
Renato Magrini <renatomagrini@gmail.com>
2021-03-02 16:01:15 +0100

    guess filter working

--- script.js
+++ script.js
@@ -2,13 +2,34 @@
 
 // ELEMENTS
 
-const guess = document.querySelector(".guess");
+const inputguess = document.querySelector(".guess");
+const palert = document.querySelector(".palert");
 
 let score, highscore, playing;
 
+// NEW GAME
+const initial = () => {
+  score = 0;
+  highscore = 0;
+  playing = true;
+};
+initial();
+
+// GUESSING
 const startGuess = () => {
-  const number = Math.floor(Math.random() * 20) + 1;
-  console.log(number);
+  if (playing) {
+    const number = Math.floor(Math.random() * 20) + 1;
+    let guess = Number(inputguess.value);
+    console.log(guess);
+
+    if (guess > 0 && guess <= 20) {
+      console.log("good to go");
+    } else {
+      inputguess.value = "";
+      inputguess.focus();
+      return;
+    }
+  }
 };
 
 document.querySelector(".guessbtn").addEventListener("click", startGuess);
